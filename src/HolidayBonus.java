134b69ef4d18ab20e007a0aafe27b8160f36520b
casulemarc <casulmarc2014@gmail.com>
2021-04-18 02:51:48 -0400

    HolidayBonus  class Update

--- src/HolidayBonus.java
+++ src/HolidayBonus.java
@@ -1,4 +1,28 @@
 
 public class HolidayBonus {
+	/**
+	 * no-arg constructor
+	 */
+	public HolidayBonus() {
+	}
+	/**
+	 * 
+	 * @param data
+	 * @param high
+	 * @param low
+	 * @param other
+	 * @return
+	 * 
+	 *         public static double[] calculateHolidayBonus(double[][] data, double
+	 *         high, double low, double other) {}
+	 * 
+	 * 
+	 *         public static double calculateTotalHolidayBonus(double[][] data,
+	 *         double high, double low, double other){}
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 
 }
